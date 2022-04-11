ba2829f2fa0c7fb3fe47e56e72d6c64f076b6a33
hobbs9458 <hobbs9458@gmail.com>
2022-04-10 21:07:36 -0500

    commit 2

--- index.js
+++ index.js
@@ -2,13 +2,53 @@ const dropdownBtnWraps = document.querySelectorAll(".dropdown-btn-wrap");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const dropdownCard = document.querySelector(".dropdown-card");
 const drowdownMenusArr = Array.from(document.querySelectorAll(".dropdown"));
+const arrowImages = document.querySelectorAll(".dropdown-arrow");
+const navDropdownBtnsDesktop = document.querySelectorAll(".dropdownBtn");
+const desktopNavDropDowns = Array.from(
+  document.querySelectorAll(".desktop-nav-dropdown")
+);
 
+// DESKTOP NAV DROPDOWN MENUS
+navDropdownBtnsDesktop.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // TOGGLE DROPDOWN ARROWS DESKTOP HELPER FUNCTION
+    const flipDesktopArrow = () => {
+      const arrow = dropdownBtn.querySelector(".dropdown-arrow");
+      arrow.classList.toggle("dropdown-arrow-image-up");
+    };
+
+    // TOGGLE DROPDOWNS
+    const dropdownBtn = e.target.closest(".dropdownBtn");
+    const dataStr = e.target.closest(".dropdownBtn").dataset.desktopdropdown;
+    const dropdown = desktopNavDropDowns.filter((dropdown) =>
+      dropdown.classList.contains(dataStr)
+    );
+
+    desktopNavDropDowns.forEach((dropdown) => {
+      if (!dropdown.classList.contains(dataStr)) {
+        dropdown.classList.add("hidden");
+      }
+    });
+
+    dropdown[0].classList.toggle("hidden");
+
+    // FLIP ARROW IMG WHEN OPENING/CLOSING DROPDOWN
+    flipDesktopArrow();
+  });
+});
+
+// MOBILE NAV HAMBURGER MENU
 hamburgerIcon.addEventListener("click", () => {
   // WHEN THEY CLOSE THE HAMBURGER MENU, ALL OF
   // THE NESTED UL DROPDOWNS SHOULDCLOSE TOO
   if (hamburgerIcon.classList.contains("closing-icon")) {
     drowdownMenusArr.forEach((menu) => {
       menu.classList.add("hidden");
+    });
+
+    // TOGGLE DROPDOWN ARROWS TO UP POSITION WHEN HAMBURGER MENU IS CLOSED
+    arrowImages.forEach((img) => {
+      img.classList.add("dropdown-arrow-image-up");
     });
   }
 
@@ -27,10 +67,11 @@ dropdownBtnWraps.forEach((btn) => {
 
     // TOGGLE TARGETED DROPDOWN MENU
     const dataStr = e.target.closest(".dropdown-btn-wrap").dataset.dropdown;
-    // const drowdownMenusArr = Array.from(document.querySelectorAll(".dropdown"));
+
     const targetMenu = drowdownMenusArr.filter((menu) => {
       return menu.classList.contains(dataStr);
     });
+
     targetMenu[0].classList.toggle("hidden");
   });
 });
